b49a39c1ec6eb3b204487cc8dba8a7e5bee423e9
hyeondk <mazy454@hanmail.net>
2022-09-03 13:47:19 +0900

    Uploaded py file used as package.

---
+++ 2. Analysis Code/4. Multiple Linear Regression(Consumption)/old_utils.py
@@ -0,0 +1,20 @@
+### Code : Function Utils
+### Writer : Donghyeon Kim
+
+# 0. 라이브러리 설정
+from pathlib import Path
+import os
+import pandas as pd
+import numpy as np
+import openpyxl
+
+# 1. 파일의 상위-상위 경로 설정
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+# 2. 사용자 이름 획득
+def get_name_root():
+    root = get_project_root()
+    folder_root = os.path.join(root, 'data') # 루트 + 'data' 경로 설정
+    user_name = os.listdir(folder_root) # 경로 안에 있는 사용자명
+    return user_name
